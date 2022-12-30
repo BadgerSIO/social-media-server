fb7695a4908909041e6f3a971dc66a2332753d21
BadgerSIO <saadibneomar@gmail.com>
2022-12-30 19:18:29 +0600

    review collections added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -28,6 +28,7 @@ const run = async () => {
   try {
     const postCollections = client.db("socialMedia").collection("posts");
     const userCollections = client.db("socialMedia").collection("users");
+    const reviewCollections = client.db("socialMedia").collection("reviews");
     //add posts
     app.post("/post", async (req, res) => {
       const post = req.body;
@@ -40,10 +41,43 @@ const run = async () => {
       const result = await postCollections.find(query).toArray();
       res.send(result);
     });
+    //get single post
+    app.get("/posts/:id", async (req, res) => {
+      const postId = req.params.id;
+      const query = {
+        _id: ObjectId(postId),
+      };
+      const result = await postCollections.findOne(query);
+      res.send(result);
+    });
     //add user
     app.post("/user", async (req, res) => {
       const user = req.body;
       const result = await userCollections.insertOne(user);
+      res.send(result);
+    });
+    //get user
+    app.get("/user/:email", async (req, res) => {
+      const reqEmail = req.params.email;
+      const query = {
+        userEmail: reqEmail,
+      };
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    });
+    //add review
+    app.post("/comment", async (req, res) => {
+      const comment = req.body;
+      const result = await reviewCollections.insertOne(comment);
+      res.send(result);
+    });
+    //get review
+    app.get("/comment/:postId", async (req, res) => {
+      const postId = req.params.postId;
+      const query = {
+        postId: postId,
+      };
+      const result = await reviewCollections.find(query).toArray();
       res.send(result);
     });
   } finally {
